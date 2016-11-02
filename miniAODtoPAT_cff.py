5a0905402c442e86b097c86562cb84471010ba00
weishi10141993 <weishi@rice.edu>
2016-11-01 20:11:05 -0500

    Add files via upload

--- miniAODtoPAT_cff.py
+++ miniAODtoPAT_cff.py
@@ -1,15 +1,19 @@
+import FWCore.ParameterSet.Config as cms
 from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
 from TrackingTools.GeomPropagators.SmartPropagator_cff import *
 from TrackingTools.MaterialEffects.MaterialPropagator_cfi import *
 from TrackingTools.MaterialEffects.OppositeMaterialPropagator_cfi import *
 from PhysicsTools.PatAlgos.patSequences_cff import *
-from PhysicsTools.PatAlgos.triggerLayer1.triggerProducer_cfi import *
-from PhysicsTools.PatAlgos.triggerLayer1.triggerEventProducer_cfi import *
+from PhysicsTools.PatAlgos.slimming.unpackedPatTrigger_cfi import unpackedPatTrigger
+#from PhysicsTools.PatAlgos.triggerLayer1.triggerProducer_cfi import *
+#from PhysicsTools.PatAlgos.triggerLayer1.triggerEventProducer_cfi import *
 #from PhysicsTools.PatAlgos.triggerLayer1.triggerMatcher_cfi import * -- deprecated in 73
+
 
 muonMatch = muonMatch.clone(
     src = cms.InputTag("slimmedMuons"),
-    resolveByMatchQuality = cms.bool(True)
+    resolveByMatchQuality = cms.bool(True),
+    matched = cms.InputTag("prunedGenParticles"),
 )
 patMuons = patMuons.clone(
     muonSource = cms.InputTag("slimmedMuons"),
@@ -29,9 +33,10 @@ patMuons = patMuons.clone(
     embedCaloMETMuonCorrs = cms.bool(False),
     embedTcMETMuonCorrs = cms.bool(False),
 )
+
 # Tracker Muons Part
 selectedPatTrackerMuons = selectedPatMuons.clone(
-    src = cms.InputTag("patMuons"),
+    src = cms.InputTag("slimmedMuons"),
     cut = cms.string("pt > 5.0 && isTrackerMuon() && numberOfMatches() > 1 && -2.4 < eta() && eta() < 2.4")
 )
 cleanPatTrackerMuons = cleanPatMuons.clone(
@@ -42,7 +47,7 @@ countPatTrackerMuons = countPatMuons.clone(
 )
 # PF Muons Part
 selectedPatPFMuons = selectedPatMuons.clone(
-    src = cms.InputTag("patMuons"),
+    src = cms.InputTag("slimmedMuons"),
     #"Loose Muon" requirement on PF muons as recommended by Muon POG:
     #https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideMuonId#Loose_Muon
     cut = cms.string("pt > 5.0 && isPFMuon() && ( isTrackerMuon() || isGlobalMuon() ) && -2.4 < eta() && eta() < 2.4")
@@ -58,7 +63,8 @@ countPatPFMuons = countPatMuons.clone(
 # This module is derived from from https://github.com/cms-sw/cmssw/blob/CMSSW_7_3_X/PhysicsTools/PatAlgos/python/triggerLayer1/triggerMatcherExamples_cfi.py
 cleanMuonTriggerMatchHLTMu17 = cms.EDProducer("PATTriggerMatcherDRDPtLessByR", # match by DeltaR only, best match by DeltaR
     src     = cms.InputTag( "cleanPatMuons" ),
-    matched = cms.InputTag( "patTrigger" ),  # default producer label as defined in PhysicsTools/PatAlgos/python/triggerLayer1/triggerProducer_cfi.py
+   # src     = cms.InputTag( "slimmedMuons" ),
+    matched = cms.InputTag( "unpackedPatTrigger" ),  # default producer label as defined in PhysicsTools/PatAlgos/python/triggerLayer1/triggerProducer_cfi.py
     matchedCuts = cms.string( 'path( "HLT_Mu17_v*" )' ),
     maxDPtRel = cms.double( 0.5 ),
     maxDeltaR = cms.double( 0.5 ),
@@ -74,26 +80,33 @@ cleanMuonTriggerMatchHLTMu17 = cms.EDProducer("PATTriggerMatcherDRDPtLessByR", #
 # This is trigger match for Tracker muons
 cleanTrackerMuonTriggerMatchHLTMu = cleanMuonTriggerMatchHLTMu17.clone(
     src = cms.InputTag( "cleanPatTrackerMuons" ),
-    matchedCuts = cms.string('path("HLT_Mu*")')
+ #  src = cms.InputTag( "slimmedMuons" ), 
+ #  matched = cms.InputTag( "selectedPatTrigger" ),
+   matchedCuts = cms.string('path("HLT_Mu*")')
 )
 cleanTrackerMuonTriggerMatchHLTIsoMu = cleanMuonTriggerMatchHLTMu17.clone(
     src = cms.InputTag( "cleanPatTrackerMuons" ),
+  #  src = cms.InputTag( "slimmedMuons" ),
     matchedCuts = cms.string('path("HLT_IsoMu*")')
 )
 cleanTrackerMuonTriggerMatchHLTDoubleMu = cleanMuonTriggerMatchHLTMu17.clone(
     src = cms.InputTag( "cleanPatTrackerMuons" ),
+   # src = cms.InputTag( "slimmedMuons" ),
     matchedCuts = cms.string('path("HLT_DoubleMu*_v*")')
 )
 cleanTrackerMuonTriggerMatchHLTTrkMu15DoubleTrkMu5 = cleanMuonTriggerMatchHLTMu17.clone(
     src = cms.InputTag( "cleanPatTrackerMuons" ),
+ #   src = cms.InputTag( "slimmedMuons" ),
     matchedCuts = cms.string('path("HLT_TrkMu15_DoubleTrkMu5NoFiltersNoVtx_v*")')
 )
 cleanTrackerMuonTriggerMatchHLTTrkMu17DoubleTrkMu8 = cleanMuonTriggerMatchHLTMu17.clone(
     src = cms.InputTag( "cleanPatTrackerMuons" ),
+ #   src = cms.InputTag( "slimmedMuons" ),
     matchedCuts = cms.string('path("HLT_TrkMu17_DoubleTrkMu8NoFiltersNoVtx_v*")')
 )
 cleanPatTrackerMuonsTriggerMatch = cms.EDProducer("PATTriggerMatchMuonEmbedder",
     src = cms.InputTag("cleanPatTrackerMuons"),
+ #   src = cms.InputTag( "slimmedMuons" ),  
     matches = cms.VInputTag("cleanTrackerMuonTriggerMatchHLTMu",
                             "cleanTrackerMuonTriggerMatchHLTIsoMu",
                             "cleanTrackerMuonTriggerMatchHLTDoubleMu",
@@ -106,26 +119,32 @@ cleanPatTrackerMuonsTriggerMatch = cms.EDProducer("PATTriggerMatchMuonEmbedder",
 # This is trigger match for PF muons
 cleanPFMuonTriggerMatchHLTMu = cleanMuonTriggerMatchHLTMu17.clone(
     src = cms.InputTag( "cleanPatPFMuons" ),
+   # src = cms.InputTag( "slimmedMuons" ),
     matchedCuts = cms.string('path("HLT_Mu*")')
 ) 
 cleanPFMuonTriggerMatchHLTIsoMu = cleanMuonTriggerMatchHLTMu17.clone(
     src = cms.InputTag( "cleanPatPFMuons" ),
+   # src = cms.InputTag( "slimmedMuons" ),  
     matchedCuts = cms.string('path("HLT_IsoMu*")')
 )
 cleanPFMuonTriggerMatchHLTDoubleMu = cleanMuonTriggerMatchHLTMu17.clone(
     src = cms.InputTag( "cleanPatPFMuons" ),
+ #   src = cms.InputTag( "slimmedMuons" ),
     matchedCuts = cms.string('path("HLT_DoubleMu*_v*")')
 )
 cleanPFMuonTriggerMatchHLTTrkMu15DoubleTrkMu5 = cleanMuonTriggerMatchHLTMu17.clone(
     src = cms.InputTag( "cleanPatPFMuons" ),
+ #   src = cms.InputTag( "slimmedMuons" ),
     matchedCuts = cms.string('path("HLT_TrkMu15_DoubleTrkMu5NoFiltersNoVtx_v*")')
 )   
 cleanPFMuonTriggerMatchHLTTrkMu17DoubleTrkMu8 = cleanMuonTriggerMatchHLTMu17.clone(
     src = cms.InputTag( "cleanPatPFMuons" ),
+   # src = cms.InputTag( "slimmedMuons" ),
     matchedCuts = cms.string('path("HLT_TrkMu17_DoubleTrkMu8NoFiltersNoVtx_v*")')
 )   
 cleanPatPFMuonsTriggerMatch = cms.EDProducer("PATTriggerMatchMuonEmbedder",
     src = cms.InputTag("cleanPatPFMuons"),
+ #   src = cms.InputTag( "slimmedMuons" ),
     matches = cms.VInputTag("cleanPFMuonTriggerMatchHLTMu",
                             "cleanPFMuonTriggerMatchHLTIsoMu",
                             "cleanPFMuonTriggerMatchHLTDoubleMu",
@@ -154,10 +173,12 @@ patifyPFMuon = cms.Sequence(
     cleanPFMuonTriggerMatchHLTTrkMu17DoubleTrkMu8 *
     cleanPatPFMuonsTriggerMatch
 )
+
 patifyData = cms.Sequence(
-    patMuons * 
-    patTrigger * 
-    patTriggerEvent * 
+ #   patMuons * 
+  #  patTrigger * 
+#    patTriggerEvent * 
+    unpackedPatTrigger *
     patifyTrackerMuon * 
     patifyPFMuon
 )
@@ -169,3 +190,4 @@ patifyMC = cms.Sequence(
 patDefaultSequence = cms.Sequence()
 patCandidates = cms.Sequence()
 makePatMuons = cms.Sequence()
+
--- patTuple_cutana_mujets_80X_cfg.py
+++ patTuple_cutana_mujets_80X_cfg.py
@@ -1,40 +1,64 @@
 ## import skeleton process
-from PhysicsTools.PatAlgos.patTemplate_cfg import *
+#from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
 # verbose flags for the PF2PAT modules
-process.options.allowUnscheduled = cms.untracked.bool(True)
+#process.options.allowUnscheduled = cms.untracked.bool(True)
+#process.options.allowUnscheduled = cms.untracked.bool(False)
+
+import FWCore.ParameterSet.Config as cms
+#process = cms.Process("MuJetProducer")
+#process = cms.Process("Demo")
+process = cms.Process("CutFlowAnalyzer")
+
+process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.Geometry.GeometryRecoDB_cff")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+#process.load("MuJetAnalysis.CutFlowAnalyzer.CutFlowAnalyzer_cfi")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+
+
+#we may need this since in patTemplate_cfg its auto:run2_mc
+process.GlobalTag = GlobalTag(process.GlobalTag, '80X_mcRun2_asymptotic_2016_miniAODv2_v1', '')
 
 ### Add MuJet Dataformats
-from MuJetAnalysis.DataFormats.EventContent_version10_cff import *
-process = customizePatOutput(process)
+#from MuJetAnalysis.DataFormats.EventContent_version10_cff import *
+#process = customizePatOutput(process)
 
 process.load("MuJetAnalysis.DataFormats.miniAODtoPAT_cff")
-process.patMuons.addGenMatch = cms.bool(True)
-process.patMuons.embedGenMatch = cms.bool(True)
+#process.patMuons.addGenMatch = cms.bool(True)
+#process.patMuons.embedGenMatch = cms.bool(True)
 
 ## pick latest HLT process
-process.patTrigger.processName = cms.string( "*" )
-process.patTriggerEvent.processName = cms.string( "*" )
+#process.patTrigger.processName = cms.string( "*" )
+#process.patTriggerEvent.processName = cms.string( "*" )
 
 process.load("MuJetAnalysis.MuJetProducer.MuJetProducer_cff")
 process.load("MuJetAnalysis.CutFlowAnalyzer.CutFlowAnalyzer_cff")
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        "file:out_miniaod.root"
-#        "file:/fdata/hepx/store/user/bmichlin/DarkSUSY_mH_125_mGammaD_0250_cT_000_Evt_79k_13TeV_MG452_BR224_LHE_pythia8_GEN_SIM_MCRUN2_71_V1_v1/DarkSUSY_mH_125_mGammaD_0250_cT_000_Evt_79k_13TeV_RAW2DIGI_L1Reco_RECO_MCRUN2_74_V9_v1/151026_194054/0000/out_reco_1.root"
+#specify in crab3 file
+        "file:out_miniaod_1.root"
+       # "file:/fdata/hepx/store/user/bmichlin/DarkSUSY_mH_125_mGammaD_0250_cT_000_Evt_79k_13TeV_MG452_BR224_LHE_pythia8_GEN_SIM_MCRUN2_71_V1_v1/DarkSUSY_mH_125_mGammaD_0250_cT_000_Evt_79k_13TeV_RAW2DIGI_L1Reco_RECO_MCRUN2_74_V9_v1/151026_194054/0000/out_reco_1.root"
     )
 )
 
-process.maxEvents.input = -1
+#process.maxEvents.input = -1
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
 
 process.p = cms.Path(
+    process.patifyMC *
     process.MuJetProducers * 
     process.cutFlowAnalyzers
 )
 
-process.outpath = cms.EndPath(process.out)
+#process.outpath = cms.EndPath(process.out)
 
 process.TFileService = cms.Service("TFileService",
-    fileName = cms.string("out_ana.root")
+    fileName = cms.string("out_ana_1.root")
 )
