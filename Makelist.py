2cb2618ab9f387af5191c8e8b36087def81e0b05
Wei Shi <weishi@rice.edu>
2018-05-10 12:57:07 +0200

    Update Makelist.py

--- Makelist.py
+++ Makelist.py
@@ -5,10 +5,10 @@ with open("Wei.dat") as f:
 # you may also want to remove whitespace characters like `\n` at the end of each line
 content = [x.strip() for x in content]
 
-sys.stdout = open('RAWFileList.py','w')
+sys.stdout = open('list_cff.py','w')
 
 for a in range(0, len(content)):
-    if "RAW" in content[a]:
+    if "315/188" in content[a]:
         if a==0:
            print "inputFileNames=['%s',"%(content[a])
         if a!=0:
