277e8fa3832dcf1322f5df80db6f9fc301da576c
Wei Shi <weishi@rice.edu>
2019-05-20 14:38:40 -0500

    Create Pythia8GeneratorFilter_13TeV_QCD_Continuum_4mu_cfi.py

---
+++ Pythia8GeneratorFilter_13TeV_QCD_Continuum_4mu_cfi.py
@@ -0,0 +1,50 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+  maxEventsToPrint = cms.untracked.int32(0),
+  pythiaPylistVerbosity = cms.untracked.int32(0),
+  filterEfficiency = cms.untracked.double(1),
+  pythiaHepMCVerbosity = cms.untracked.bool(False),
+  comEnergy = cms.double(13000.0),
+  PythiaParameters = cms.PSet(
+    pythia8CommonSettingsBlock,
+    pythia8CP5SettingsBlock,
+    processParameters = cms.vstring(
+    'ParticleDecays:limitTau0 = off',
+    'ParticleDecays:limitCylinder = on',
+    'ParticleDecays:xyMax = 2000',
+    'ParticleDecays:zMax = 4000',
+    'HardQCD:all = on',
+    'PhaseSpace:pTHatMin = 20',
+    'PhaseSpace:pTHatMax = 9999',
+    '130:mayDecay = on',
+    '211:mayDecay = on',
+    '321:mayDecay = on'
+    ),
+    parameterSets = cms.vstring('pythia8CommonSettings',
+      'pythia8CP5Settings',
+      'processParameters',
+    )
+  )
+)
+
+mugenfilter = cms.EDFilter("MCSmartSingleParticleFilter",
+      MinPt = cms.untracked.vdouble(15., 15.),
+      MinEta = cms.untracked.vdouble(-3.0, -3.0),
+      MaxEta = cms.untracked.vdouble(3.0, 3.0),
+      ParticleID = cms.untracked.vint32(13, -13),
+      Status = cms.untracked.vint32(1, 1),
+      MaxDecayRadius = cms.untracked.vdouble(2000., 2000.),
+      MinDecayZ = cms.untracked.vdouble(-4000., -4000.),
+      MaxDecayZ = cms.untracked.vdouble(4000., 4000.),
+#      BetaBoost = cms.untracked.double(-0.434)
+)
+
+configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('PYTHIA 8 QCD in pp (pt-hat 20 GeV) at sqrt(s) = 13 TeV')
+)
+
+ProductionFilterSequence = cms.Sequence(generator*mugenfilter)
