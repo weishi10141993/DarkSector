05af1aef39bd004f45133fc861607d21a7870f4d
Wei Shi <weishi@rice.edu>
2019-05-23 12:13:38 -0500

    Add files via upload

---
+++ Pythia8HadronizerFilter_13TeV_cfi_Tau0Off.py
@@ -0,0 +1,26 @@
+## Pythia8 fragment for 13TeV collisions
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(True),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    crossSection = cms.untracked.double(0.0),
+    comEnergy = cms.double(13000.0),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters = cms.vstring(
+        'ParticleDecays:limitTau0 = off',
+        ),
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CP5Settings',
+            'processParameters',
+        )
+    )
+)
