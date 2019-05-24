b8f4eb1dda47ca6d886354f649b303784d2ced16
Wei Shi <weishi@rice.edu>
2019-05-24 13:20:20 -0500

    Update Pythia8HadronizerFilter_MSSMD_13TeV_cfi.py

--- Pythia8HadronizerFilter_MSSMD_13TeV_cfi.py
+++ Pythia8HadronizerFilter_MSSMD_13TeV_cfi.py
@@ -15,9 +15,10 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
         pythia8CommonSettingsBlock,
         pythia8CP5SettingsBlock,
         processParameters = cms.vstring(
+            'SLHA:allowUserOverride = on',
             'ParticleDecays:tau0Max = 1000.1',
             'LesHouches:setLifetime = 2', #Ignore lifetime setting in LHE
-            '3000022:all = ad void 3 0 0 5.0 0.00001 0.0 0.0 50.0', #Set lifetime in Pythia8
+            '3000022:all = ad void 3 0 0 5.0 0.00001 0.0 0.0 50.0' #Set lifetime in Pythia8
         ),
         parameterSets = cms.vstring(
             'pythia8CommonSettings',
